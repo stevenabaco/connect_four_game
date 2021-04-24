3aebd637410436f21f5797884ebd0f1157f45a11
Steven Abaco <stevenabaco@gmail.com>
2021-04-24 14:41:53 -0500

    Add createSpaces Method to Board Class Constructor

--- js/Board.js
+++ js/Board.js
@@ -1,7 +1,28 @@
 class Board {
-  constructor() {
-    this.rows = 6;
-    this.columns = 7;
-    this.spaces = [];
-  }
-};
+	constructor() {
+		this.rows = 6;
+		this.columns = 7;
+		this.spaces = this.createSpaces();
+	}
+
+	/**
+	 * Generates 2D array of spaces.
+	 * @return  {Array}     An array of space objects
+	 */
+	createSpaces() {
+		const spaces = [];
+
+		for (let x = 0; x < this.columns; x++) {
+			const column = [];
+
+			for (let y = 0; y < this.rows; y++) {
+				const space = new space(x, y);
+				column.push(column);
+			}
+
+			spaces.push(column);
+		}
+
+		return spaces;
+	}
+}
