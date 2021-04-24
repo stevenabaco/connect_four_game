bb64fb62c0e3d9181097ae897aa9cc5022350bbb
Steven Abaco <stevenabaco@gmail.com>
2021-04-24 15:05:18 -0500

    Add Game Class and creatMethod

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,20 @@
-class Game { };
+class Game {
+	constructor(board, players) {
+		this.board = new Board();
+		this.players = this.createPlayers();
+		this.ready = false;
+	}
+
+	/**
+	 * Creates two player objects
+	 * @return  {Array}    An array of two Player objects.
+	 */
+
+	createPlayers() {
+    const players = [
+      new Player('Player 1', 1, '#e15258', true),
+      new Player('Player 2', 3, '#e59a13')
+    ];
+    return players;
+	}
+}
