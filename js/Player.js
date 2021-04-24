4a3f1593da63812e21c45c1592271f22e42076eb
Steven Abaco <stevenabaco@gmail.com>
2021-04-24 13:48:52 -0500

    Add Player class

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,9 @@
 class Player {
-  
-};
+	constructor(player, id, color, active = false) {
+    this.player = player;
+    this.id = id;
+    this.color = color;
+    this.active = active;
+    this.tokens = [];
+	}
+}
