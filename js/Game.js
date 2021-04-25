c03312c9cc6ce73a89365dce50f59c08a5832d4f
Steven Abaco <stevenabaco@gmail.com>
2021-04-25 09:45:20 -0500

    Add token getter methods

--- js/Game.js
+++ js/Game.js
@@ -1,19 +1,18 @@
 class Game {
-	constructor(board, players) {
-		this.board = new Board();
-		this.players = this.createPlayers();
-		this.ready = false;
-	}
-
-	/**
-	 * Creates two player objects
-	 * @return  {Array}    An array of two Player objects.
-	 */
-
-	createPlayers() {
+  constructor(board, players) {
+    this.board = new Board();
+    this.players = this.createPlayers();
+    this.ready = false;
+  }
+  
+  /**
+   * Creates two player objects
+   * @return  {Array}    An array of two Player objects.
+   */
+  createPlayers() {
     const players = [
       new Player('Player 1', 1, '#e15258', true),
-      new Player('Player 2', 3, '#e59a13')
+      new Player('Player 2', 3, '#e59a13'),
     ];
     return players;
   }
@@ -24,4 +23,22 @@ class Game {
   startGame() {
     
   };
-}
+
+  /**
+   * Gets all tokens that haven't been dropped.
+   * @return {array} Array of unused tokens.
+   */
+  get unusedTokens() {
+    return this.tokens.filter(token => !token.dropped);
+  }
+
+  /**
+   * Gets active tokens by returning first token in the array of unused tokens.
+   * @return {Object} First token object in the array of unused tokens.
+   */
+
+  get activeToken() {
+    return this.unusedTokens[0]
+  }
+
+};
